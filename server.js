12726bcf457ead1eab2e571d3f408876193a4cda
Katrin Junolainen <katrin.junolainen@gmail.com>
2025-04-06 10:06:44 +0300

    add week10 code

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+
+    
+    let url = 'https://api.themoviedb.org/3/movie/558449?api_key=4ca94f8b470d7e34bd3f59c3914295c8';
+    axios.get(url)
+    .then(response => {
+        console.log(response.data.title);
+        let data = response.data;
+        let releaseDate = new Date(data.release_date).getFullYear();
+        let genres = '';
+
+        data.genres.forEach(genre => {
+            genres = genres + `${genre.name}, `;
+        });
+
+        
+        let genresUpdated = genres.slice(0, -2) + '.';
+        moviePoster = `https://image.tmdb.org/t/p/w1280${data.poster_path }`;
+        console.log(genresUpdated);
+        let currentYear = new Date().getFullYear();
+        res.render('index', {movieData: data, releaseDate: releaseDate, genres: genresUpdated, poster: moviePoster, year: currentYear});
+    });
+    
+})
+
+
+
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('server is running');
+});
